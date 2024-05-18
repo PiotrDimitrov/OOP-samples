8304fddbef22d62dffdba004be1243e76161c52a
PiotrDimitrov <piotr.dimitrov2019@gmail.com>
2024-05-18 23:59:49 +0300

    access modifiers: private, public, protected

--- accessModifier.cpp
+++ accessModifier.cpp
@@ -1,30 +1,22 @@
 #include <iostream>
 
-class Human {
-public:
-    std::string name;
-    int age;
-    int weight;
+namespace acsmdf{
+    class Point {
+    private:
+        int x;
+    protected:
+        int y;
+    public:
+        int z;
+        void Print(){ //inside function can access private and protected
+            std::cout << x << " " << y << " " << z << std::endl;
+        }
+    };
+}
 
-};
 
-class Point {
-private:
-    int x;
-
-protected:
-    int y;
-
-public:
-    int z;
-
-    void Print(){ //inside function can access private and protected
-        std::cout << x << " " << y << " " << z << std::endl;
-    }
-};
-
-int main() {
-    Point a;
+void accessModifier() {
+    acsmdf::Point a;
     //a.x = 1; //cannot access private
     //cout << a.x;
     //a.y = 2; //cannot access protected
