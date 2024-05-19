c8fae40b96f26ebd483a145561076889eb769784
PiotrDimitrov <piotr.dimitrov2019@gmail.com>
2024-05-19 14:19:01 +0300

    friend class method

--- friendClassMethod.cpp
+++ friendClassMethod.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 #include <string>
 
-namespace fcmth{
-    class Human;
-    class Apple;// Forward declaration
+namespace frmth{
+    class Apple; // Forward declaration
 
     class Human {
     private:
         std::string name;
     public:
-        void takeApple(Apple& a) {
-            //std::cout << "Human " << name << " took apple with weight " << a.weight << " and color " << a.color << std::endl;
-        }
+        void takeApple(Apple& a); // Declare the function here, but do not define it
     };
 
     class Apple {
     private:
         int weight;
         std::string color;
-        //friend void Human::takeApple(Apple a);
+
+        friend void Human::takeApple(Apple& a);
     };
+    void Human::takeApple(Apple& a) { // Define the function here
+        std::cout << "Human " << name << " took apple with weight " << a.weight << " and color " << a.color << std::endl;
+    }
 }
 
-void friendClassMethod() {
+int main() {
     std::cout << "Hello World!\n";
 }
